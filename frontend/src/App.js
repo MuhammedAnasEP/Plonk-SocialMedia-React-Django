3e54dd849d1e8e9914336f54d6a199f68ebef449
MuhammedAnasEP <anasep1110@gmail.com>
2023-04-19 11:14:31 +0530

    created react app for frontend development and clean the app next i installed tailwindcss

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Login from "./Components/Login"
+function App() {
+  return (
+    <div>
+      <Login></Login>
+      <h1 className="text-red-500">i am for testing</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Login.js
@@ -0,0 +1,7 @@
+function Login(){
+    return(
+        <div>This is a login page</div>
+    )
+}
+
+export default Login
